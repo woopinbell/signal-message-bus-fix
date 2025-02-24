543dbff51b6ef36f7d960086e8c4a8bcee3e21dd
seungwoo7050 <seungwoo7050@naver.com>
2025-02-24 10:10:03 +0900

    refactor(server): 시그널 처리를 self-pipe 이벤트 루프로 전환

--- src/server.c
+++ src/server.c
@@ -4,6 +4,7 @@
 
 #include <errno.h>
 #include <fcntl.h>
+#include <limits.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/select.h>
@@ -12,35 +13,35 @@
 #include <sys/un.h>
 #include <unistd.h>
 
-typedef struct s_response_request
+typedef struct s_bit_event
 {
-	pid_t		client_pid;
-	uint32_t	kind;
-	uint32_t	token;
-	int32_t		status;
-}	t_response_request;
+	pid_t	sender;
+	int		signal;
+}	t_bit_event;
 
-static volatile sig_atomic_t	g_current_byte;
-static volatile sig_atomic_t	g_received_bits;
-static volatile sig_atomic_t	g_client_pid;
-static volatile sig_atomic_t	g_line_started;
-static volatile sig_atomic_t	g_response_overflow;
-static volatile sig_atomic_t	g_sequence;
-static int					g_response_pipe[2] = {-1, -1};
-static int					g_response_socket = -1;
-static int					g_server_bound;
-static char					g_server_path[MT_RESPONSE_PATH_SIZE];
+typedef char	t_event_must_fit_pipe_buf[(sizeof(t_bit_event) <= PIPE_BUF) * 2 - 1];
+
+static unsigned char			g_current_byte;
+static unsigned int			g_received_bits;
+static pid_t					g_client_pid;
+static int						g_line_started;
+static uint32_t				g_sequence;
+static volatile sig_atomic_t	g_event_overflow;
+static int						g_event_pipe[2] = {-1, -1};
+static int						g_response_socket = -1;
+static int						g_server_bound;
+static char						g_server_path[MT_RESPONSE_PATH_SIZE];
 
 static void	cleanup_server(void)
 {
-	if (g_response_pipe[0] != -1)
-		close(g_response_pipe[0]);
-	if (g_response_pipe[1] != -1)
-		close(g_response_pipe[1]);
+	if (g_event_pipe[0] != -1)
+		close(g_event_pipe[0]);
+	if (g_event_pipe[1] != -1)
+		close(g_event_pipe[1]);
 	if (g_response_socket != -1)
 		close(g_response_socket);
-	g_response_pipe[0] = -1;
-	g_response_pipe[1] = -1;
+	g_event_pipe[0] = -1;
+	g_event_pipe[1] = -1;
 	g_response_socket = -1;
 	if (g_server_bound && g_server_path[0] != '\0')
 		unlink(g_server_path);
@@ -73,53 +74,20 @@ static void	flush_byte(unsigned char output)
 	}
 }
 
-static void	queue_response(pid_t client_pid, uint32_t kind,
-		uint32_t token, int status)
-{
-	t_response_request	request;
-
-	request.client_pid = client_pid;
-	request.kind = kind;
-	request.token = token;
-	request.status = status;
-	if (write(g_response_pipe[1], &request, sizeof(request))
-		!= (ssize_t)sizeof(request))
-		g_response_overflow = 1;
-}
-
 static void	handle_bit(int signal, siginfo_t *info, void *context)
 {
-	unsigned char	output;
-	uint32_t		sequence;
+	t_bit_event	event;
 	int				saved_errno;
 
 	saved_errno = errno;
 	(void)context;
-	if (info == NULL || info->si_pid <= 0)
-	{
-		errno = saved_errno;
-		return ;
-	}
-	if (g_client_pid == 0 || g_client_pid != info->si_pid)
-	{
-		errno = saved_errno;
-		return ;
-	}
-	sequence = (uint32_t)g_sequence;
-	g_current_byte <<= 1;
-	if (signal == MT_ONE_SIGNAL)
-		g_current_byte |= 1;
-	g_received_bits++;
-	if (g_received_bits == 8)
-	{
-		output = (unsigned char)g_current_byte;
-		flush_byte(output);
-		g_current_byte = 0;
-		g_received_bits = 0;
-	}
-	if (g_client_pid != 0)
-		g_sequence++;
-	queue_response(info->si_pid, MT_RESPONSE_ACK, sequence, MT_RESPONSE_OK);
+	event.sender = 0;
+	if (info != NULL)
+		event.sender = info->si_pid;
+	event.signal = signal;
+	if (write(g_event_pipe[1], &event, sizeof(event))
+		!= (ssize_t)sizeof(event))
+		g_event_overflow = 1;
 	errno = saved_errno;
 }
 
@@ -130,6 +98,16 @@ static int	set_nonblocking_close_on_exec(int fd)
 	flags = fcntl(fd, F_GETFL);
 	if (flags == -1 || fcntl(fd, F_SETFL, flags | O_NONBLOCK) == -1)
 		return (-1);
+	flags = fcntl(fd, F_GETFD);
+	if (flags == -1 || fcntl(fd, F_SETFD, flags | FD_CLOEXEC) == -1)
+		return (-1);
+	return (0);
+}
+
+static int	set_close_on_exec(int fd)
+{
+	int	flags;
+
 	flags = fcntl(fd, F_GETFD);
 	if (flags == -1 || fcntl(fd, F_SETFD, flags | FD_CLOEXEC) == -1)
 		return (-1);
@@ -158,8 +136,9 @@ static int	prepare_response_channel(void)
 {
 	struct sockaddr_un	address;
 
-	if (pipe(g_response_pipe) == -1
-		|| set_nonblocking_close_on_exec(g_response_pipe[1]) == -1)
+	if (pipe(g_event_pipe) == -1
+		|| set_close_on_exec(g_event_pipe[0]) == -1
+		|| set_nonblocking_close_on_exec(g_event_pipe[1]) == -1)
 		return (-1);
 	if (mt_response_path(g_server_path, sizeof(g_server_path), "server",
 			getpid()) == -1 || remove_stale_socket(g_server_path) == -1)
@@ -187,14 +166,14 @@ static int	install_signal_handlers(void)
 {
 	struct sigaction	action;
 
+	memset(&action, 0, sizeof(action));
 	action.sa_sigaction = handle_bit;
 	sigemptyset(&action.sa_mask);
 	sigaddset(&action.sa_mask, MT_ZERO_SIGNAL);
 	sigaddset(&action.sa_mask, MT_ONE_SIGNAL);
 	action.sa_flags = SA_SIGINFO;
-	if (sigaction(MT_ZERO_SIGNAL, &action, NULL) == -1)
-		return (-1);
-	if (sigaction(MT_ONE_SIGNAL, &action, NULL) == -1)
+	if (sigaction(MT_ZERO_SIGNAL, &action, NULL) == -1
+		|| sigaction(MT_ONE_SIGNAL, &action, NULL) == -1)
 		return (-1);
 	return (0);
 }
@@ -213,14 +192,15 @@ static int	valid_client_socket(const char *path)
 	return (0);
 }
 
-static int	send_response(const t_response_request *request)
+static int	send_response(pid_t client_pid, uint32_t kind, uint32_t token,
+		int status)
 {
 	struct sockaddr_un	address;
 	t_mt_response		response;
 	char				client_path[MT_RESPONSE_PATH_SIZE];
 
 	if (mt_response_path(client_path, sizeof(client_path), "client",
-			request->client_pid) == -1 || valid_client_socket(client_path) == -1)
+			client_pid) == -1 || valid_client_socket(client_path) == -1)
 		return (-1);
 	memset(&address, 0, sizeof(address));
 	address.sun_family = AF_UNIX;
@@ -231,9 +211,9 @@ static int	send_response(const t_response_request *request)
 	}
 	memcpy(address.sun_path, client_path, mt_strlen(client_path) + 1);
 	response.magic = MT_RESPONSE_MAGIC;
-	response.kind = request->kind;
-	response.token = request->token;
-	response.status = request->status;
+	response.kind = kind;
+	response.token = token;
+	response.status = status;
 	response.server_pid = getpid();
 	if (sendto(g_response_socket, &response, sizeof(response), 0,
 			(struct sockaddr *)&address, sizeof(address))
@@ -284,77 +264,119 @@ static int	read_session_request(t_mt_request *request)
 
 static int	handle_session_request(void)
 {
-	t_response_request	response;
-	t_mt_request		request;
-	int					new_owner;
-	int					status;
+	t_mt_request	request;
+	int				new_owner;
+	int				status;
 
 	status = read_session_request(&request);
 	if (status <= 0)
 		return (status);
 	new_owner = 0;
-	response.status = MT_RESPONSE_OK;
+	status = MT_RESPONSE_OK;
 	if (g_client_pid != 0 && g_client_pid != request.client_pid)
 	{
-		if (kill((pid_t)g_client_pid, 0) == -1 && errno == ESRCH)
+		if (kill(g_client_pid, 0) == -1 && errno == ESRCH)
 			reset_session(1);
 		else
-			response.status = MT_RESPONSE_BUSY;
+			status = MT_RESPONSE_BUSY;
 	}
-	if (response.status == MT_RESPONSE_OK && g_client_pid == 0)
+	if (status == MT_RESPONSE_OK && g_client_pid == 0)
 	{
 		g_client_pid = request.client_pid;
 		new_owner = 1;
 	}
-	response.client_pid = request.client_pid;
-	response.kind = MT_RESPONSE_READY;
-	response.token = request.nonce;
-	if (send_response(&response) == -1 && new_owner)
+	if (send_response(request.client_pid, MT_RESPONSE_READY, request.nonce,
+			status) == -1 && new_owner)
 		reset_session(0);
 	return (0);
 }
 
-static int	respond_to_bit(void)
+static int	read_event(t_bit_event *event)
 {
-	t_response_request	request;
-	ssize_t				size;
+	unsigned char	*bytes;
+	size_t			offset;
+	ssize_t			size;
 
-	size = read(g_response_pipe[0], &request, sizeof(request));
-	if (size == -1 && errno == EINTR)
-		return (0);
-	if (size != (ssize_t)sizeof(request) || g_response_overflow)
-		return (-1);
-	if (send_response(&request) == -1 && request.status == MT_RESPONSE_OK
-		&& request.client_pid == (pid_t)g_client_pid)
-		reset_session(1);
+	bytes = (unsigned char *)event;
+	offset = 0;
+	while (offset < sizeof(*event))
+	{
+		size = read(g_event_pipe[0], bytes + offset, sizeof(*event) - offset);
+		if (size == -1 && errno == EINTR)
+			continue ;
+		if (size <= 0)
+			return (-1);
+		offset += (size_t)size;
+	}
 	return (0);
 }
 
-static int	run_response_loop(void)
+static int	process_bit(const t_bit_event *event)
 {
-	fd_set	read_set;
-	int		max_fd;
-	int		status;
+	unsigned char	output;
+	uint32_t		sequence;
 
-	max_fd = g_response_pipe[0];
+	if (event->sender <= 0
+		|| (event->signal != MT_ZERO_SIGNAL && event->signal != MT_ONE_SIGNAL))
+		return (0);
+	if (g_client_pid == 0 || g_client_pid != event->sender)
+		return (0);
+	sequence = g_sequence;
+	g_current_byte <<= 1;
+	if (event->signal == MT_ONE_SIGNAL)
+		g_current_byte |= 1;
+	g_received_bits++;
+	if (g_received_bits == 8)
+	{
+		output = g_current_byte;
+		flush_byte(output);
+		g_current_byte = 0;
+		g_received_bits = 0;
+	}
+	if (g_client_pid != 0)
+		g_sequence++;
+	if (send_response(event->sender, MT_RESPONSE_ACK, sequence, MT_RESPONSE_OK) == -1)
+	{
+		if (event->sender == g_client_pid)
+			reset_session(1);
+	}
+	return (0);
+}
+
+static int	run_event_loop(void)
+{
+	t_bit_event	event;
+	fd_set		read_set;
+	int			max_fd;
+	int			status;
+
+	max_fd = g_event_pipe[0];
 	if (g_response_socket > max_fd)
 		max_fd = g_response_socket;
 	while (1)
 	{
+		if (g_event_overflow)
+		{
+			errno = ENOBUFS;
+			return (-1);
+		}
 		FD_ZERO(&read_set);
-		FD_SET(g_response_pipe[0], &read_set);
+		FD_SET(g_event_pipe[0], &read_set);
 		FD_SET(g_response_socket, &read_set);
 		status = pselect(max_fd + 1, &read_set, NULL, NULL, NULL, NULL);
 		if (status == -1 && errno == EINTR)
 			continue ;
-		if (status == -1 || g_response_overflow)
+		if (status == -1)
 			return (-1);
 		if (FD_ISSET(g_response_socket, &read_set)
 			&& handle_session_request() == -1)
 			return (-1);
-		if (FD_ISSET(g_response_pipe[0], &read_set)
-			&& respond_to_bit() == -1)
-			return (-1);
+		if (FD_ISSET(g_event_pipe[0], &read_set))
+		{
+			if (read_event(&event) == -1 || g_event_overflow
+				|| process_bit(&event) == -1)
+				return (-1);
+		}
 	}
 }
 
@@ -375,9 +397,9 @@ int	main(void)
 	}
 	mt_putnbr_fd(getpid(), STDOUT_FILENO);
 	write(STDOUT_FILENO, "\n", 1);
-	if (run_response_loop() == -1)
+	if (run_event_loop() == -1)
 	{
-		mt_putstr_fd("server: response channel failed\n", STDERR_FILENO);
+		mt_putstr_fd("server: signal event channel failed\n", STDERR_FILENO);
 		return (1);
 	}
 	return (0);
